aa79ff22b1f5506b33ea010aff3be9047a0e0444
codemohitpra03 <codemohitprakanojia2103@gmail.com>
2024-02-12 22:09:56 +0530

    Changed Log function

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,7 +1,7 @@
 const loggingMiddleware = (req, res, next) => {
     const timestamp = new Date().toISOString();
     const { method, url, headers } = req;
-    const accessToken = headers['authorization'].split(" ")[1]; // Assuming the access token is provided in the 'Authorization' header
+    const accessToken = headers['authorization']; // Assuming the access token is provided in the 'Authorization' header
 
     console.log(`[${timestamp}] ${method}: ${url}, AccessToken: "${accessToken}"`);
 
